7226cb575632f07b93f63aebb18874b7e235eb49
Grzegorz Miszewski <grzegorz@miszewski.net.pl>
2012-04-10 18:49:21 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tabu::Application.config.secret_token = '836352147ca5d3925802c028b28b0ef47d5a01b74f446596308f4855b6ad3806f0bfc5824f97566afdcd356df6c1575ffe2ab4b4d0c04b5ab59a667c13cf403c'
