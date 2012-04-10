2a1094c9dda93ebb40d117f9dabfd2e59179b873
Grzegorz Miszewski <miszmaniac@gmail.com>
2012-04-10 20:39:07 +0200

    Added static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
-    it "should have the content 'Tabu'" do
+    it "should have the content 'Strona glowna'" do
       visit '/static_pages/home'
 
       page.should have_content('Strona glowna')
+    end
+  end
+  describe "Help page" do
+
+    it "should have the content 'Pomoc'" do
+      visit '/static_pages/help'
+      page.should have_content('pomoc')
     end
   end
 end
