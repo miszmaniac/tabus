75de6725c751e02032cab3474a8af1dcf0457758
Grzegorz Miszewski <miszmaniac@gmail.com>
2012-04-15 18:21:00 +0200

    Modyfikacja testów

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,31 +4,52 @@ require 'spec_helper'
 describe "StaticPages" do
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1', text: 'Tabu') }
-    it { should have_content('Tabu') }
-    it { should have_selector('title', :text => full_title(''))}
-    it { should_not have_selector('title', :text => "Strona główna") }
+    let(:heading) { 'Tabu' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: 'Strona główna | ' }
+
+    it "should have the right links on the layout" do
+      visit root_path
+
+      click_link "O stronie"
+      page.should have_selector 'title', text: full_title('O stronie')
+    end
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('pomoc') }
+    let(:heading) { 'Pomoc' }
+    let(:page_title) { 'Pomoc' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Privacy policy" do
     before { visit privacy_path }
 
-    it { should have_content('Polityka prywatności') }
+    let(:heading) { 'Polityka prywatności' }
+    let(:page_title) { 'Polityka prywatności' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before {visit contact_path }
 
-    it { should have_selector('h1', text: 'Kontakt') }
-    it { should have_selector('title', text: full_title('Kontakt')) }
+    let(:heading) { 'Kontakt' }
+    let(:page_title) { 'Kontakt' }
+
+    it_should_behave_like "all static pages"
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,1 @@
-def full_title(page_title)
-  base_title = "Tabu - najlepsza gra towarzyska"
-  if page_title.empty?
-    base_title
-  else
-    "#{page_title} | #{base_title}"
-  end
-end
+include ApplicationHelper
