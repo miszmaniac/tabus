b82820197e517c09532c1902e5698e620391850f
Grzegorz Miszewski <miszmaniac@gmail.com>
2012-04-19 21:08:25 +0200

    Examples

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,6 +17,12 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      describe "has error message" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Załóż konto') }
+        it { should have_content("error") }
+      end
     end
 
     describe "with valid information" do
@@ -30,7 +36,17 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: User.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Witamy') }
+      end
     end
+
+
   end
 
   describe "profile page" do
